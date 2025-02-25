273e6553c4e44f23478d1b53939d3dad43f7acbc
pavan1 <pavankumarpilaka96@gmail.com>
2025-02-25 21:16:36 +0530

    added all the imp routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Home from './pages/home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import Contact from './pages/Contact'
+import Myprofile from './pages/Myprofile'
+import Myappointments from './pages/Myappointments'
+import Appointments from './pages/Appointments'
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/doctors' element={<Doctors/>} />
+        <Route path='/doctors/:speciality' element={<Doctors/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/my-profile' element={<Myprofile/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/my-appointments' element={<Myappointments/>} />
+        <Route path='/appointement/:docId' element={<Appointments/>} />
       </Routes>
     </div>
   )
